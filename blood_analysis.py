cd6a68219696fd4a8b15ee64021356996c998c71
Abinaya Lakshmanan <al325@duke.edu>
2020-05-20 09:37:13 -0500

    Add HDL analysis to base interface

--- blood_analysis.py
+++ blood_analysis.py
@@ -1,14 +1,20 @@
 # blood_analysis.py
 
+def HDL_interface():
+    pass
+
 def interface():
     print("My Blood Analysis Calculator")
     keep_running = True
     while keep_running:
-        print("Options: ")
+        print("\nOptions: ")
+        print("1-HDL analysis")
         print("9-Quit")
         choice = input("Choose an option: ")
         if choice == '9':
             keep_running = False
+        elif choice == '1':
+            HDL_interface()
 
 if __name__ == "__main__":
     interface()
