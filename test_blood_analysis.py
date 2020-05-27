9e598b7664c91f09c8e2b1989cd361ce8ee79fca
Abinaya Lakshmanan <al325@duke.edu>
2020-05-27 09:31:37 -0500

    updated testing for Travis

--- test_blood_analysis.py
+++ test_blood_analysis.py
@@ -4,11 +4,13 @@ def test_HDL_analysis ():
     expected = "Good"
     assert answer == expected #if assert true, test will pass or else test will fail
 
+
 def test_HDL_analysis_borderline():
     from blood_analysis import HDL_analysis
     answer = HDL_analysis(45)
     expected = "Borderline"
     assert answer == expected
+
 
 def test_HDL_analysis_bad():
     from blood_analysis import HDL_analysis
@@ -16,11 +18,13 @@ def test_HDL_analysis_bad():
     expected = "Bad"
     assert answer == expected
 
+
 def test_LDL_analysis():
     from blood_analysis import LDL_analysis
     answer = LDL_analysis(120)
     expected = "Normal"
     assert answer == expected
+
 
 def test_line_generation():
     from blood_analysis import line_generation
@@ -28,10 +32,12 @@ def test_line_generation():
     expected = 2
     assert answer == expected
 
+
 def test_third_point():
     from blood_analysis import third_point
     answer = third_point((1,1),(2,2),(3,3))
     assert answer == True
+
 
 def test_third_point_wrong():
     from blood_analysis import third_point
